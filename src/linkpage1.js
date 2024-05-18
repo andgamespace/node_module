83753155f5b52f9c5ce112ddc49c043820546ce4
andgamespace <anshxc@gmail.com>
2024-05-18 12:14:08 -0500

    farthest I've been

---
+++ src/linkpage1.js
@@ -0,0 +1,6 @@
+export function loadLink1(){
+    const contentDiv = document.getElementById("content");
+    //const contentDiv2 = document.createElement("div");
+    //contentDiv2.setAttribute("id", "contentDiv2");
+    
+}
---
+++ src/script.js
@@ -0,0 +1,21 @@
+import { loadLink1 } from "./linkpage1.js";
+
+function main() {
+    const button1 = document.createElement("button");
+    const button2 = document.createElement("button");
+    button1.setAttribute("id", "button1");
+    button2.setAttribute("id", "button2");
+    const contentDiv = document.getElementById("content");
+    //        <button id = "button1"></button>
+    //        <button id = "button2"></button>
+
+}
+var refresh = false;
+// function refresh(){
+//     if(refresh){
+//         $('#content'). empty();
+//     }
+// }
+window.addEventListener = (onload) => {
+    main();
+}
